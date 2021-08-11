e0de60aea6ce8b0ba2902adf908d3f156a570f99
Geoffrey Dijkstra <geoffrey.dijkstra@gmail.com>
2021-08-11 10:32:42 +0200

    Removed DI again.
    DI did not work within phar files?
    At least for now just removed it so app keeps working.

--- bin/console.php
+++ bin/console.php
@@ -1,9 +1,24 @@
 <?php
 
-use App\Services\Application;
+use App\Commands\Assignee;
+use App\Commands\BranchNameConvention;
+use App\Commands\CommitMessages;
+use App\Commands\Labels;
+use App\Services\CodeOwnersFile;
+use App\Services\GithubActionConfig;
+use App\Services\GithubApiCommands;
+use Symfony\Component\Console\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Application('Devorto Commit Messages', '1.0.0');
-$app->addDirectory(__DIR__ . '/../src/Commands', 'App\Commands');
+
+$config = new GithubActionConfig();
+$commands = new GithubApiCommands($config);
+$owner = new CodeOwnersFile();
+
+$app->add(new Assignee($config, $commands));
+$app->add(new BranchNameConvention($config, $commands, $owner));
+$app->add(new CommitMessages($config, $commands));
+$app->add(new Labels($commands, $config));
 $app->run();
